5c835b8f75e517b167b5b0189c8756664c821d4e
AndrianoTurner <danya.shibaev@gmail.com>
2025-01-06 22:48:31 +0300

    add send_to_ollama arm to send_request

--- src/crates/services/src/llm_api/mod.rs
+++ src/crates/services/src/llm_api/mod.rs
@@ -1,12 +1,14 @@
 use alta_s_api::send_to_altas;
 use groq_api::send_to_groq;
+use ollama_api::send_to_ollama;
+use tracing::*;
 use reqwest::Client;
 use shared::{configuration::CONFIG, types::AiRecognizeMethod};
 use tracing::*;
 
 mod alta_s_api;
 mod groq_api;
-
+mod ollama_api;
 // todo: покрыть все ошибки, а не те которые мне по кайфу щас
 #[derive(Debug)]
 pub enum AiRequestError {
@@ -14,6 +16,7 @@ pub enum AiRequestError {
     GroqRequest,
     AltaSUrl,
     AltaSRequest,
+    OllamaRequest
 }
 
 // todo: rewrite to result
@@ -29,6 +32,7 @@ pub async fn send_request(req: String) -> Result<String, AiRequestError> {
             }
         }
         AiRecognizeMethod::AltaS => send_to_altas(req).await,
+        AiRecognizeMethod::Ollama => send_to_ollama(&req).await,
         AiRecognizeMethod::None => Err(AiRequestError::GroqRequest), // nothing for recognize, so just return command
     }
 }
