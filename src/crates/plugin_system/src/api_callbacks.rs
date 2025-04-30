deccb339c7c51350d58b6f747cb2674c57ba9d97
Дмитрий Прудников <d.prudniko@madrigal.expert>
2025-04-30 15:55:56 +0300

    some improvemens

--- src/crates/plugin_system/src/api_callbacks.rs
+++ src/crates/plugin_system/src/api_callbacks.rs
@@ -4,13 +4,13 @@ use std::ffi::{c_char, c_void, CString};
 
 use tracing::*;
 
-use crate::event_system;
+use crate::{event_system, ReadableRequest};
 
-use super::{abstractions, ReadableRequest};
+use super::abstractions;
 
 pub unsafe fn get_api() -> ApiCallbacksMap {
     let callbacks = vec![
-        // ApiCallbacksPair::new("send_human_request", send_human_request as *const c_void),
+        ApiCallbacksPair::new("send_human_request", send_human_request as *const c_void),
         ApiCallbacksPair::new("subscribe_to_events", subscribe_to_events as *const c_void),
         ApiCallbacksPair::new("publish_event", publish_event as *const c_void),
     ];
@@ -22,22 +22,22 @@ lazy_static! {
     static ref RUNTIME: tokio::runtime::Runtime = tokio::runtime::Runtime::new().unwrap();
 }
 
-// #[no_mangle]
-// unsafe extern "C" fn send_human_request(human: *mut c_char) {
-//     let from_raw = CString::from_raw(human);
-//     RUNTIME.spawn(async move {
-//         let cstring_cast = from_raw.to_str();
-//         match cstring_cast {
-//             Ok(casted_str) => {
-//                 event_system::publish(ReadableRequest {
-//                     request: casted_str.to_string(),
-//                 })
-//                 .await;
-//             }
-//             Err(err) => warn!("Error due send_human_response API call: {}", err),
-//         }
-//     });
-// }
+#[no_mangle]
+unsafe extern "C" fn send_human_request(human: *mut c_char) {
+    let from_raw = CString::from_raw(human);
+    RUNTIME.spawn(async move {
+        let cstring_cast = from_raw.to_str();
+        match cstring_cast {
+            Ok(casted_str) => {
+                event_system::publish(ReadableRequest {
+                    request: casted_str.to_string(),
+                })
+                .await;
+            }
+            Err(err) => warn!("Error due send_human_response API call: {}", err),
+        }
+    });
+}
 
 #[no_mangle]
 unsafe extern "C" fn subscribe_to_events(callback: unsafe extern "C" fn(*const c_char)) {
@@ -48,6 +48,7 @@ unsafe extern "C" fn subscribe_to_events(callback: unsafe extern "C" fn(*const c
             let mut lock = rx.lock().await;
             let res = lock.recv().await.unwrap();
             let s = CString::new(res).unwrap();
+            println!("Received event: {}", s.to_str().unwrap());
             callback(s.into_raw().cast_const());
         }
     });
